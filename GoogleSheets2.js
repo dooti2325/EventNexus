c7318f98b9a35de5799e9a4623b30db1befb4160
dooti2325 <dootisaha.2325@gmail.com>
2024-11-13 10:12:08 +0530

    Normal

--- GoogleSheets2.js
+++ GoogleSheets2.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxtQkVmK53QX7jbD4oDdGc1lmK8XwGZkL7XOsPXLGj3jSvxkOZK7Nx7MoHqUqnJlnlg/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxdicK_PivSP4gJj177sydPwD-qRsU4o-vZ5vKeFZQTPPqYHie5MGVDE3Y7KaZNx0hL/exec'
 
 
 const form = document.forms['coding-workshop']
